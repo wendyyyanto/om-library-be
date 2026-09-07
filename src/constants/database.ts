b2db71cc84e96a2539bcc9ce80f2d0a3cf85e524
wendyyyanto <wendyyyanto@gmail.com>
2026-09-07 12:21:46 +0700

    adjust libraryfiles and teachings entity

--- src/constants/database.ts
+++ src/constants/database.ts
@@ -1,13 +1,13 @@
-import { LibraryFileEntity } from '../entities/LibraryFileEntity';
-import { LibraryRoleEntity } from '../entities/LibraryRoleEntity';
-import { LibraryStatusEntity } from '../entities/LibraryStatusEntity';
-import { LibraryUserEntity } from '../entities/LibraryUserEntity';
-import { TeachingEntity } from '../entities/TeachingEntity';
+import { LibraryFileEntity } from "../entities/LibraryFileEntity";
+import { LibraryRoleEntity } from "../entities/LibraryRoleEntity";
+import { LibraryStatusEntity } from "../entities/LibraryStatusEntity";
+import { LibraryUserEntity } from "../entities/LibraryUserEntity";
+import { TeachingEntity } from "../entities/TeachingEntity";
 
 export const DATABASE_ENTITIES = [
-  LibraryUserEntity,
-  LibraryFileEntity,
-  LibraryRoleEntity,
-  LibraryStatusEntity,
-  TeachingEntity,
+	LibraryUserEntity,
+	LibraryFileEntity,
+	LibraryRoleEntity,
+	LibraryStatusEntity,
+	TeachingEntity
 ];
